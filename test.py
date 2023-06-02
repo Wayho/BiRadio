922030c240e5eba97abb98c64ee98d7b170f56ae
Wayho <34697764+Wayho@users.noreply.github.com>
2023-06-02 09:56:25 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import eyed3
+import os
+import random
+from datetime import datetime
+
+if __name__ == '__main__':
+    d = datetime.now().weekday()
+    print(d)
+
+# cmdlist = cmdlist()
+# for cmd in cmdlist:
+#     print(cmd)

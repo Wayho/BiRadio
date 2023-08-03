4f36baa28381ace0e5684e1c3befbcfe79bf9235
Wayho <34697764+Wayho@users.noreply.github.com>
2023-08-03 14:41:24 +0800

    if 'frame='!=last_msg[0:6]:

--- shell.py
+++ shell.py
@@ -6,11 +6,68 @@ import psutil
 import sys
 from queue import Queue
 
-MAX_MSG_NUM = 30
+MAX_MSG_NUM = 25
 ignore_msgs = ['mp3float','Last message','frame=','configuration:']
-print('shell v5.5.2',ignore_msgs)
+print('shell v5.5.5',MAX_MSG_NUM,ignore_msgs)
 ##################################################
 def OutputShell( cmd, msgout=True ):
+	# if print last 20 msg
+	msg_queue_obj = Queue(MAX_MSG_NUM)  # 创建一个队列对象
+	print( 'shell:',msgout,cmd[0:400])
+	result = subprocess.Popen(
+		#[ "ping 127.0.0.1" ],
+		#[ "find /usr" ],
+		[ cmd ],
+		shell=True,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE
+	)
+	# read date from pipe
+	select_rfds = [ result.stdout, result.stderr ]
+	last_msg = ''
+	last_errmsg = ''
+	while len( select_rfds ) > 0:
+		(rfds, wfds, efds) = select.select( select_rfds, [ ], [ ] ) #select函数阻塞进程，直到select_rfds中的套接字被触发
+		if result.stdout in rfds:
+			readbuf_msg = result.stdout.readline()      #行缓冲
+			if len( readbuf_msg ) == 0:
+				select_rfds.remove( result.stdout )     #result.stdout需要remove，否则进程不会结束
+			else:
+				#print( readbuf_msg.decode("utf-8"))
+				try:
+					last_msg = str(readbuf_msg, 'utf8')
+					if msgout:
+							if 'frame='!=last_msg[0:6]:
+								print(last_msg,end='')
+					else:
+						msg_queue_obj = fifo_msg(msg_queue_obj,last_msg)
+				except:
+					msg_queue_obj = fifo_msg(msg_queue_obj,'OutputShell:error last_msg utf8')
+
+		if result.stderr in rfds:
+			readbuf_errmsg = result.stderr.readline()
+			if len( readbuf_errmsg ) == 0:
+				select_rfds.remove( result.stderr )     #result.stderr，否则进程不会结束
+			else:
+				try:
+					last_errmsg = str(readbuf_errmsg, 'utf8')
+					if msgout:
+							if 'frame='!=last_errmsg[0:6]:
+								print(last_errmsg,end='')
+					else:
+						msg_queue_obj = fifo_msg(msg_queue_obj,last_errmsg)
+				except:
+					msg_queue_obj = fifo_msg(msg_queue_obj,'OutputShell:error readbuf_errmsg utf8')
+	result.wait() # 等待字进程结束( 等待shell命令结束 )
+	#print result.returncode
+	##(stdoutMsg,stderrMsg) = result .communicate()#非阻塞时读法.
+	if not msgout:
+		while not msg_queue_obj.empty():
+			print('last_msg:',msg_queue_obj.get(),end='')
+	return result.returncode
+
+##################################################
+def OutputShell_Old( cmd, msgout=True ):
 	# output twice ignore_msgs
 	# if print last 20 msg
 	ignore_msgs_times = [0,0,0,0]

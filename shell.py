5f8b5399e47229177c52c2b2c1cbe5a86da88591
Wayho <34697764+Wayho@users.noreply.github.com>
2023-08-13 16:35:45 +0800

    Update shell.py

--- shell.py
+++ shell.py
@@ -105,7 +105,7 @@ def OutputShell( cmd, msgout=True ):
 								if '[flv @'!=last_errmsg[0:6]:
 									print(last_errmsg,end='')
 					else:
-						if 'frame= 50'==last_msg[0:9]:
+						if 'frame= 50'==last_errmsg[0:9]:
 							msg_queue_obj = fifo_msg(msg_queue_obj,'EE'+last_errmsg)
 						else:
 						        msg_queue_obj = fifo_msg(msg_queue_obj,last_errmsg)

d15fe41794ac3f6bacc61019b6e1a26a621207e9
Wayho <34697764+Wayho@users.noreply.github.com>
2023-08-21 15:16:07 +0800

    Add files via upload

--- class_voice.py
+++ class_voice.py
@@ -29,7 +29,7 @@ Global_danmu_play = ['好想听','好想播','好想放','好想','想听','听'
 Global_danmu_how = [
     ]
 
-print('voice v5.9.5 DB_NAME:',DB_NAME)
+print('voice v5.9.6 DB_NAME:',DB_NAME)
 # https://peiyin.xunfei.cn/make
 # https://peiyin.xunfei.cn/synth?uid=211119012301271462&ts=1691561751&sign=a20ff619b322943058f72f7eaae4ae6f&vid=60140&f=v2&cc=0000&listen=0&sid=211119012301271462&volume=-20&speed=38&content=%5Bte50%5D%E6%AC%A2%E8%BF%8E%E6%9D%A5%E5%88%B0%E6%88%91%E7%9A%84%E7%9B%B4%E6%92%AD%E9%97%B4&normal=1
 # 玲姐姐 语速 50    l1001.m4a
@@ -40,7 +40,7 @@ def get_amix_voice():
     :return:  {'hasmsg':True/Flase,'mp4':None,'amix':amix}
     :hasmsg:  有无消息，用于前端的随机处理，hasmsg与mp4没有关系
     :mp4:  点的歌
-    :amix:  语音obj，肯定不为空, {'type':voice.get('type'),'text':voice.get('text'),'path':file_path}
+    :amix:  语音obj，肯定不为空, {'type':voice.get('type'),'gift_id':voice.get('gift_id'),'text':voice.get('text'),'path':file_path}
     """
     global Global_like
     global Global_danmu
@@ -64,7 +64,7 @@ def get_amix_voice():
         return process_danmu()
     if len(Global_like)!=0:
         return process_like()
-    amix = [{'type':10,'text':'欢迎来到宝宝的直播间','path':os.path.join(SOURCE_VOICE_FLODER, AMIX_DEFLAULT)}]
+    amix = [{'type':10,'gift_id':0,'text':'欢迎来到宝宝的直播间','path':os.path.join(SOURCE_VOICE_FLODER, AMIX_DEFLAULT)}]
     return {'hasmsg':False,'mp4':None,'amix':amix}
 
 def process_like():
@@ -80,22 +80,22 @@ def process_like():
         elif msg.get('type') == wss_danmu.MsgType.INTERACT_WORD:
             interact = True
     if like:
-        voice_arr = find_voice(20)
+        voice_arr = find_voice(20,gift_id=0)
         if voice_arr:
             amix = [voice_arr[0]]
     elif interact:
-        voice_arr = find_voice(10)
+        voice_arr = find_voice(10,gift_id=0)
         if voice_arr:
             amix = [voice_arr[0]]
     if not amix:
-        amix = [{'type':10,'text':'欢迎来到宝宝的直播间','path':os.path.join(SOURCE_VOICE_FLODER, AMIX_DEFLAULT)}]
+        amix = [{'type':10,'gift_id':0,'text':'欢迎来到宝宝的直播间','path':os.path.join(SOURCE_VOICE_FLODER, AMIX_DEFLAULT)}]
     return  {'hasmsg':hasmsg,'mp4':None,'amix':amix}
 
 def process_danmu():
     global Global_danmu
     hasmsg = False
     mp4 = None
-    amix = [{'type':31,'text':'喜欢的话可以点歌，下一首播','path':os.path.join(SOURCE_VOICE_FLODER, 'l3100.m4a')}]
+    amix = [{'type':31,'gift_id':0,'text':'喜欢的话可以点歌，下一首播','path':os.path.join(SOURCE_VOICE_FLODER, 'l3100.m4a')}]
     ret = {}
     for msg in Global_danmu:
         hasmsg = True
@@ -110,13 +110,13 @@ def process_danmu():
                 if subtitle:
                     # 数据库找到歌
                     mp4 = subtitle.get('name') + '.mp4'
-                    voice_arr = find_voice(33)
+                    voice_arr = find_voice(33,gift_id=0)
                     if voice_arr:
                         amix = voice_arr
                     return {'hasmsg':hasmsg,'mp4':mp4,'amix':amix}
                 else:
                     # 消息没处理完，不着急返回，先准备一个返回值备用
-                    amix = [{'type':333,'text':'这首歌没有哦我换首歌送给您！','path':os.path.join(SOURCE_VOICE_FLODER, 'l3300.m4a')}]
+                    amix = [{'type':333,'gift_id':0,'text':'这首歌没有哦我换首歌送给您！','path':os.path.join(SOURCE_VOICE_FLODER, 'l3300.m4a')}]
                     ret = {'hasmsg':hasmsg,'mp4':mp4,'amix':amix}
         # can't shipei paly,try title
         title = danmu
@@ -125,13 +125,13 @@ def process_danmu():
         if subtitle:
             # 数据库找到歌
             mp4 = subtitle.get('name') + '.mp4'
-            voice_arr = find_voice(33)
+            voice_arr = find_voice(33,gift_id=0)
             if voice_arr:
                 amix = voice_arr
             return {'hasmsg':hasmsg,'mp4':mp4,'amix':amix}
     if not ret:
         # 弹幕没点歌，也说点啥
-        voice_arr = find_voice(7)   # 某段歌词语音
+        voice_arr = find_voice(7,gift_id=0)   # 某段歌词语音
         if voice_arr:
             amix = [voice_arr[0],amix[0]]
         ret = {'hasmsg':hasmsg,'mp4':mp4,'amix':amix}
@@ -144,37 +144,37 @@ def process_gift():
         for msg in Global_gift:
             #print('#'*40,'GIFT message={}'.format(msg.get('message')))
             gift_id = msg.get('message').gift_id
-            voice_arr = find_voice(gift_id)
+            voice_arr = find_voice(4,gift_id)
             if voice_arr:
-                if not is_type_in_amix(voice_arr[0].get('type'),amix):
+                if not is_gift_id_in_amix(voice_arr[0].get('gift_id'),amix):
                     # 一种礼物留一个语音
                     amix.append(voice_arr[0])
     except:
         print('gift error')
     if not amix:
         # 没有匹配的语音，用默认
-        amix = [{'type':40,'text':'谢谢宝宝的礼物','path':os.path.join(SOURCE_VOICE_FLODER, 'l4009.m4a')}]
+        amix = [{'type':4,'gift_id':30000,'text':'谢谢宝宝的礼物','path':os.path.join(SOURCE_VOICE_FLODER, 'l4009.m4a')}]
     return  {'hasmsg':True,'mp4':None,'amix':amix}
 
-def is_type_in_amix(itype,amix):
+def is_gift_id_in_amix(gift_id,amix):
     for a_obj in amix:
-        if itype == a_obj.get('type'):
+        if gift_id == a_obj.get('gift_id'):
             return True
     return False
 
-def find_voice(itype):
+def find_voice(itype,gift_id=0):
     """
     读数据库，看看语音文件在不在，返回在的列表，已处理好fullpath
-    :return: voice_arr {'type':voice.get('type'),'text':voice.get('text'),'path':file_path}
+    :return: voice_arr {'type':voice.get('type'),'gift_id':voice.get('gift_id'),'text':voice.get('text'),'path':file_path}
     """
     global Global_voice_obj_array
-    find = load_voice_by_type(itype)
+    find = load_voice_by_type(itype,gift_id)
     voice_arr = []
     print('find voice',len(find))
     for voice in find:
         file_path = os.path.join(SOURCE_VOICE_FLODER, voice.get('m4a'))
         if  os.path.exists(file_path):
-            voice_arr.append({'type':voice.get('type'),'text':voice.get('text'),'path':file_path})
+            voice_arr.append({'type':voice.get('type'),'gift_id':voice.get('gift_id'),'text':voice.get('text'),'path':file_path})
     random.shuffle(voice_arr)
     return voice_arr
 
@@ -186,13 +186,17 @@ def find_curl():
     return query.find()
 
     
-def load_voice_by_type(type):
+def load_voice_by_type(itype,gift_id):
+    # gift type == 4
     global Global_voice_obj_array
     DBClass = leancloud.Object.extend( DB_NAME )
     query = DBClass.query
     query.limit(100)
     #query.equal_to('on', True)
-    query.equal_to('type', type)
+    if 4==itype:
+        query.equal_to('gift_id', gift_id)
+    else:
+        query.equal_to('type', itype)
     return query.find()
 
 def load_voice():
--- stream.py
+++ stream.py
@@ -76,7 +76,7 @@ ffmpeg_rtmp_mp4 = "ffmpeg -re -i {} -r {} {} -hide_banner -f flv  {}"
 print('stream v5.7.1:mp4',ffmpeg_mp4)
 print('stream v5.4.5:rtmp',ffmpeg_playlist)
 print('stream v5.6.11:ffmpeg_looplist',ffmpeg_looplist)
-print('stream v5.9.3:ffmpeg_rtmp_mp4',ffmpeg_rtmp_mp4)
+print('stream v5.9.5:ffmpeg_rtmp_mp4',ffmpeg_rtmp_mp4)
 Global_Amix_Queue = Queue()  # 创建一个队列对象
 ##############################################
 # # 以第一个视频分辨率作为全局分辨率
@@ -292,7 +292,7 @@ def process_amix(amix,times=3):
        ret.append(a)
     for i in range(times):
         for a in amix:
-            if a.get('type')>=30000 or a.get('type')==1:
+            if a.get('type')==4 or a.get('type')==8:
                 # gift voice, record in queue
                 Global_Amix_Queue.put(a)
     return ret

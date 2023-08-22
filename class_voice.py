e7ff1e7cb8362730e981eca4da972ee54a9f43ca
Wayho <34697764+Wayho@users.noreply.github.com>
2023-08-22 11:03:35 +0800

    Add files via upload

--- class_voice.py
+++ class_voice.py
@@ -20,7 +20,7 @@ Global_gift = []
 #{"time":int,"uid":uid,"uname":uname,"type":MsgType,"message":diff type}好想听，好喜欢，超想听，超爱，超想
 Global_danmu_play = ['好想听','好想播','好想放','好想','想听','听',
             '好喜欢','喜欢','超想听','超想','超爱',
-            '麻烦播放','麻烦播送','麻烦播','麻烦点歌','麻烦点',
+            '麻烦播放','麻烦播送','麻烦播','麻烦点歌','麻烦点','麻烦',
             '请播放','请播送','请播','请点歌','请点',
             '点歌','点','播放','播送','播',
             '能不能点歌','能不能点','能不能播放','能不能播送','能不能播','能不能听','能不能放',
@@ -29,7 +29,7 @@ Global_danmu_play = ['好想听','好想播','好想放','好想','想听','听'
 Global_danmu_how = [
     ]
 
-print('voice v5.9.6 DB_NAME:',DB_NAME)
+print('voice v5.9.8 DB_NAME:',DB_NAME)
 # https://peiyin.xunfei.cn/make
 # https://peiyin.xunfei.cn/synth?uid=211119012301271462&ts=1691561751&sign=a20ff619b322943058f72f7eaae4ae6f&vid=60140&f=v2&cc=0000&listen=0&sid=211119012301271462&volume=-20&speed=38&content=%5Bte50%5D%E6%AC%A2%E8%BF%8E%E6%9D%A5%E5%88%B0%E6%88%91%E7%9A%84%E7%9B%B4%E6%92%AD%E9%97%B4&normal=1
 # 玲姐姐 语速 50    l1001.m4a
@@ -37,6 +37,8 @@ print('voice v5.9.6 DB_NAME:',DB_NAME)
 def get_amix_voice():
     """
     分别按送礼、弹幕、点赞读取弹幕消息，只处理按价值最高的几条消息
+    先说欢迎，然后感谢，然后点歌
+    如果只有欢迎，在前面加歌词
     :return:  {'hasmsg':True/Flase,'mp4':None,'amix':amix}
     :hasmsg:  有无消息，用于前端的随机处理，hasmsg与mp4没有关系
     :mp4:  点的歌
@@ -45,45 +47,55 @@ def get_amix_voice():
     global Global_like
     global Global_danmu
     global Global_gift
-    danmu = False
-    gift = False
+    amix = []
+    hasmsg = False
+    mp4 = None
+    danmu =  {'hasmsg':False,'mp4':None,'amix':[]}
+    gift =  {'hasmsg':False,'mp4':None,'amix':[]}
+    like =  {'hasmsg':False,'mp4':None,'amix':[]}
     Global_like = wss_danmu.get_like_all()
     Global_danmu = wss_danmu.get_danmu_all()
     Global_gift = wss_danmu.get_gift_all()
-    print(Global_gift)
-    print(Global_danmu)
-    print(Global_like)
+    print(Global_gift,Global_danmu,Global_like)
     #从价值最高开始处理，danmu分开出来计
+    # mp4 按danmu
     # 先不管谁，重复类型留一个
-    if len(Global_gift)!=0:
-        Global_gift.sort(key=lambda x:x['type'].value, reverse=True)
-        gift = True
-        return process_gift()
-    if len(Global_danmu)!=0:
-        danmu = True
-        return process_danmu()
-    if len(Global_like)!=0:
-        return process_like()
-    amix = [{'type':10,'gift_id':0,'text':'欢迎来到宝宝的直播间','path':os.path.join(SOURCE_VOICE_FLODER, AMIX_DEFLAULT)}]
-    return {'hasmsg':False,'mp4':None,'amix':amix}
+    interact = process_interact()
+    gift = process_gift()
+    like = process_like()
+    danmu = process_danmu()
+    if interact.get('hasmsg'):
+        hasmsg = True
+        amix.append(interact.get('amix')[0])
+    if gift.get('hasmsg'):
+        hasmsg = True
+        for am in gift.get('amix'):
+            amix.append(am)
+    if like.get('hasmsg'):
+        hasmsg = True
+        amix.append(am)
+    if danmu.get('hasmsg'):
+        hasmsg = True
+        amix.append(am)
+    if not hasmsg:
+        # 必须有一个音频
+        amix = [{'type':11,'gift_id':0,'text':'都不说话，那我唱歌了','path':os.path.join(SOURCE_VOICE_FLODER, 'l1105.m4a')}]
+    if len(amix) == 1:
+        # 也说点啥
+        voice_arr = find_voice(7,gift_id=0)   # 某段歌词语音
+        if voice_arr:
+            amix = [voice_arr[0],amix[0]]
+    return {'hasmsg':hasmsg,'mp4':mp4,'amix':amix}
 
-def process_like():
+def process_interact():
+    # INTERACT_WORD:hasmsg = False
     global Global_like
     hasmsg = False
-    interact = False
-    like = False
     amix = []
     for msg in Global_like:
-        hasmsg = True
-        if msg.get('type') == wss_danmu.MsgType.LIKE_INFO_V3_CLICK:
-            like = True
-        elif msg.get('type') == wss_danmu.MsgType.INTERACT_WORD:
-            interact = True
-    if like:
-        voice_arr = find_voice(20,gift_id=0)
-        if voice_arr:
-            amix = [voice_arr[0]]
-    elif interact:
+        if msg.get('type') == wss_danmu.MsgType.INTERACT_WORD:
+            hasmsg = True
+    if hasmsg:
         voice_arr = find_voice(10,gift_id=0)
         if voice_arr:
             amix = [voice_arr[0]]
@@ -91,12 +103,28 @@ def process_like():
         amix = [{'type':10,'gift_id':0,'text':'欢迎来到宝宝的直播间','path':os.path.join(SOURCE_VOICE_FLODER, AMIX_DEFLAULT)}]
     return  {'hasmsg':hasmsg,'mp4':None,'amix':amix}
 
+def process_like():
+    global Global_like
+    hasmsg = False
+    amix = []
+    for msg in Global_like:
+        if msg.get('type') == wss_danmu.MsgType.LIKE_INFO_V3_CLICK:
+            hasmsg = True
+    if hasmsg:
+        voice_arr = find_voice(20,gift_id=0)
+        if voice_arr:
+            amix = [voice_arr[0]]
+    if not amix:
+        amix = [{'type':20,'gift_id':0,'text':'谢谢宝宝的点赞','path':os.path.join(SOURCE_VOICE_FLODER, 'l2002')}]
+    return  {'hasmsg':hasmsg,'mp4':None,'amix':amix}
+
 def process_danmu():
+    #amix只有一个内容
     global Global_danmu
     hasmsg = False
     mp4 = None
     amix = [{'type':31,'gift_id':0,'text':'喜欢的话可以点歌，下一首播','path':os.path.join(SOURCE_VOICE_FLODER, 'l3100.m4a')}]
-    ret = {}
+    ret = {'hasmsg':False,'mp4':None,'amix':[]}
     for msg in Global_danmu:
         hasmsg = True
         danmu = msg.get('message').msg
@@ -118,7 +146,7 @@ def process_danmu():
                     # 消息没处理完，不着急返回，先准备一个返回值备用
                     amix = [{'type':333,'gift_id':0,'text':'这首歌没有哦我换首歌送给您！','path':os.path.join(SOURCE_VOICE_FLODER, 'l3300.m4a')}]
                     ret = {'hasmsg':hasmsg,'mp4':mp4,'amix':amix}
-        # can't shipei paly,try title
+        # 不能适配关键词，试试直接歌名
         title = danmu
         print(keyword,danmu,title)
         subtitle = class_subtitle.get_m4a_name(title)
@@ -129,7 +157,7 @@ def process_danmu():
             if voice_arr:
                 amix = voice_arr
             return {'hasmsg':hasmsg,'mp4':mp4,'amix':amix}
-    if not ret:
+    if hasmsg:
         # 弹幕没点歌，也说点啥
         voice_arr = find_voice(7,gift_id=0)   # 某段歌词语音
         if voice_arr:
@@ -139,9 +167,12 @@ def process_danmu():
 
 def process_gift():
     global Global_gift
+    hasmsg = False
     amix = []
+    #Global_gift.sort(key=lambda x:x['type'].value, reverse=True)
     try:
         for msg in Global_gift:
+            hasmsg = True
             #print('#'*40,'GIFT message={}'.format(msg.get('message')))
             gift_id = msg.get('message').gift_id
             voice_arr = find_voice(4,gift_id)
@@ -154,7 +185,7 @@ def process_gift():
     if not amix:
         # 没有匹配的语音，用默认
         amix = [{'type':4,'gift_id':30000,'text':'谢谢宝宝的礼物','path':os.path.join(SOURCE_VOICE_FLODER, 'l4009.m4a')}]
-    return  {'hasmsg':True,'mp4':None,'amix':amix}
+    return  {'hasmsg':hasmsg,'mp4':None,'amix':amix}
 
 def is_gift_id_in_amix(gift_id,amix):
     for a_obj in amix:
